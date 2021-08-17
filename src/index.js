ba14372afbdb6a04ba5b847cbdce976a4cc2bef6
medi772425 <75742857+medi772425@users.noreply.github.com>
2021-08-18 07:08:29 +0900

    完了・削除ボタンのイベント追加

--- src/index.js
+++ src/index.js
@@ -7,8 +7,50 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
-  div.innerText = inputText;
 
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // ------------------------------------------------------------------
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // 先祖要素のliを取得
+    const moveTarget = completeButton.closest("li").querySelector("p");
+    div.appendChild(moveTarget);
+
+    // 戻るボタン作成
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻す";
+    div.appendChild(returnButton);
+
+    // liに追加
+    li.appendChild(div);
+
+    // 完了したtodoに表示
+    document.getElementById("complete-list").appendChild(li);
+
+    const removeTarget = completeButton.closest("li");
+
+    document.getElementById("incomplete-list").removeChild(removeTarget);
+  });
+
+  // ------------------------------------------------------------------
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 先祖要素のliを取得
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   document.getElementById("incomplete-list").appendChild(li);
